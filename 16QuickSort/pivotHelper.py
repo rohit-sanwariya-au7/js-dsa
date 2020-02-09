2733e77592575b3d84f00a623d6265c017663983
rohitsanwariyaIT <rohitsanwariya1995@gmail.com>
2020-02-09 08:43:03 +0530

    pivotHelperOptimised

--- 16QuickSort/pivotHelper.py
+++ 16QuickSort/pivotHelper.py
@@ -1,16 +1,12 @@
-def swapHelper(array,pivotIndex,i):
-    array[pivotIndex],array[i]=array[i],array[pivotIndex]
-
-
-
 def pivotHelper(array):
     pivotIndex=0
+    start=0
     for i in range(1,len(array)):
-        if array[0]>array[i]:
+        if array[start]>array[i]:
             pivotIndex+=1
-            print(pivotIndex,i)
-            swapHelper(array,pivotIndex,i)
+            array[pivotIndex],array[i]=array[i],array[pivotIndex]
             print(array)
-    swapHelper(array,pivotIndex,0)
-    return array
+    array[pivotIndex],array[0]=array[0],array[pivotIndex]
+    return (array)
+    
 print(pivotHelper([4,8,7,2,2,2,3,5,1]))
