a8522580a0c228b0a0d4c08cc709deaf35fdce3a
rohitsanwariyaIT <rohitsanwariya1995@gmail.com>
2020-02-11 15:09:12 -0500

    debug mechanism

--- 13Insertion Sort/insertionSort.py
+++ 13Insertion Sort/insertionSort.py
@@ -3,13 +3,14 @@ def insertSort(array):
         current=array[i]
         j=i-1
         print("current and j",current,array[j])
+        print(array,i)
         while(j>=0 and array[j]>current):
             array[j+1]=array[j] #bhen chod jab tak array[j]>array[j+1]
             #swap karo matlab piche se ka ek pakad ke age walon se compare
             #karo har jo bada ho use swa
             j-=1
         array[j+1]=current
-        print(array,i)
+        
 
 
     return array
